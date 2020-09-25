4245b0cfd69a33bc763c17423e0e00c76906f1b8
Efabien <fabien@topteam.tech>
2020-09-25 14:05:16 +0300

    remove folder icon

--- modules/lists.js
+++ modules/lists.js
@@ -31,7 +31,7 @@ const logFiles = (files) => {
 };
 
 const logFolderName = (name) => {
-  console.log(chalk.bold.blue(` 📂 ${name} --`));
+  console.log(chalk.bold.blue(` -- ${name} --`));
 };
 
 const logFileName = (name, index) => {
